6f2def1c1565345015bc24c7e8d69b67ea453d99
PAO <natthachai_bubpa@cmu.ac.th>
2024-01-04 23:11:24 +0700

    yes!!!!!!!!!!!

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,51 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+
+    double sum = 0;
+    double max = 0;
+    double min = 0;
+    float sumdot = 0;
+    float std=0;
+    float sumha=0;
+
+    for(int i=0;i<N;i++){
+        sum += A[i];
+    }
+    double mean = sum/N;
+    B[0] = sum/N;
+
+    for(int i = 0; i < N; i++) {
+        std += pow(A[i] - mean, 2);
+    }
+    B[1]= sqrt(std/N) ;
+
+    for(int i=0;i<N;i++){
+        if(max<A[i]){
+            max = A[i];
+        }
+    }
+
+    for (int i = 0; i < N; i++){
+    sumdot = sumdot + log(A[i]);
+    }
+
+    sumdot = sumdot / N;
+    B[2] = exp(sumdot);
+
+    for (int i = 0; i < N; i++){ 
+      sumha = sumha + (float)1 / A[i];
+    }
+    B[3] = (float)N/sumha;
+    min=max;
+    for(int i=0;i<N;i++){
+        if(min>A[i]){
+            min = A[i];
+        }
+    }
+
+    B[4] = max;
+    B[5] = min;
+
+}
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,49 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool im[][M],int S,int X,int Y){
+    im[X][Y] = 1;
+    for(int i=0;i<30;i++){
+               
+            for(int j=0;j<70;j++){
+               
+                if(sqrt(pow(i-X,2)+pow(j-Y,2)) <= S-1){
+                    im[i][j] = 1;
+                }
+            
+            } 
+        
+    } 
+    
+}
+
+
+void showImage(const bool im[][M]){
+     for(int i=1;i<73;i++){
+        cout<<"-";
+        
+    } 
+    cout << endl;
+    for(int i=0;i<30;i++){
+        cout<<"|";           
+            for(int j=0;j<70;j++){
+                if(im[i][j] == 0){
+                    cout<<" ";
+                }
+                else if(im[i][j] == 1){
+                    cout<<"*";
+                }
+                
+            
+            } 
+        cout<<"|"; 
+        cout << endl;
+    } 
+    for(int i=1;i<73;i++){
+        cout<<"-";
+        
+    } 
+    cout << endl;
+    
+}
